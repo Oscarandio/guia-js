04b7fc93e11efaa11d9c66a4e6fa69bda1b02a8d
Óscar Rabadán Martínez <rabadanoscar@hotmail.com>
2023-11-08 10:37:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+//* Variables
+
+let a = 3;
+let b = 0,
+  c = 'hello'; // Comentarios
+
+//* 7 tipos PRIMITIVOS + OBJETOS + FUNCIONES
+
+//? string
+('hello'); // Comillas Simples
+('hello'); // Comillas dobles
+
+//? number
+let positive = 100; // entero positivo
+let negative = -200; // entero negativo
+let float = 1220.32; // flotante
+let exponencial = 1e6; // notacion exponencial (1 x 10^6)
+let infinity = Infinity; // infinito
+let nan = NaN; // Not a number (de hecho es de tipo número)
